c17b8cfad1079f70411d72c118eafe638fd3ce01
Dirag Biswas <36959928+diragb@users.noreply.github.com>
2018-03-16 16:21:19 +0530

    Loads JSON and saves poster.
    Makes use of json2.js, which pretty darn neat.
    
    Loads the JSON file, and runs a loop. 
    On each loop, it reads the information present in the JSON file, and decides which layer to make visible, depending on the writeup type, specified in the JSON file. 
    
    Convention: 
    1: Shortie
    2: Scribble
    3: Poem
    4: Story
    
    After it has added the content from the JSON file to the selected layers, it (function processWriteup) runs the saveJPEG function to save the current canvas in JPEG, in a child folder named 'Output'.
    Thereafter, it proceeds to make all layers invisible, and patiently waits for the next loop iteration in the main function, to generate another writeup.

---
+++ scriptPS.jsx
@@ -0,0 +1,101 @@
+/*
+[+] Change the text.
+[+] Save JPEG
+[+] Read JSON
+*/
+#include json2.js
+
+
+(function main() {
+    
+    var writeups = loadJson('test.json');
+    for (var i=0; i<writeups.length; i++) {
+        var writeup = writeups[i];
+        processWriteup(writeup);
+    }
+})();
+
+function processWriteup(writeup) {
+    var doc = app.activeDocument;
+    
+    var toEditGroup = doc.layerSets.getByName('toEdit');
+    var titleLayer = toEditGroup.layers.getByName('title');
+    var writerLayer = toEditGroup.layers.getByName('writer');
+    
+    var shortieGroup = toEditGroup.layerSets.getByName('shortie');                //Select the Shortie Group
+    var scribbleGroup = toEditGroup.layerSets.getByName('scribble');              //Select the Scribble Group
+    var poemGroup = toEditGroup.layerSets.getByName('poem');                      //Select the Poem Group
+    var storyGroup = toEditGroup.layerSets.getByName('story');                    //Select the Story Group
+    
+    hideLayers();                                                                 //Hide all the layers
+    
+    titleLayer.textItem.contents = writeup.title;                                 //Add the Title of the writeup.
+    writerLayer.textItem.contents = 'by ' + writeup.writer;                       //Add the Writer of the writeup.
+    
+    if (writeup.type == 1) {
+        var content = shortieGroup.layers.getByName('shortieContent');
+        content.textItem.contents = writeup.content;
+        shortieGroup.visible = true;
+        titleLayer.visible = true;
+        writerLayer.visible = true;
+    }
+    
+    else if (writeup.type == 2) {
+        var content = scribbleGroup.layers.getByName('scribbleContent');
+        content.textItem.contents = writeup.content;
+        scribbleGroup.visible = true;
+        titleLayer.visible = true;
+        writerLayer.visible = true;
+    }
+    
+    else if (writeup.type == 3) {
+        var content = poemGroup.layers.getByName('poemContent');
+        content.textItem.contents = writeup.content;
+        poemGroup.visible = true;
+        titleLayer.visible = true;
+        writerLayer.visible = true;
+    }
+    
+    else if (writeup.type == 4) {
+        var content = storyGroup.layers.getByName('storyContent');
+        content.textItem.contents = writeup.content;
+        storyGroup.visible = true;
+        titleLayer.visible = true;
+        writerLayer.visible = true;
+    }
+    
+    saveJPEG(writeup.id);
+    hideLayers();
+    
+    function hideLayers() {
+        shortieGroup.visible = false;
+        scribbleGroup.visible = false;
+        poemGroup.visible = false;
+        storyGroup.visible = false;
+        
+        titleLayer.visible = false;
+        writerLayer.visible = false;
+    }
+}
+
+
+function loadJson(relPath) {
+    var script = new File($.fileName);
+    var jsonFile = new File(script.path + '/' + relPath);
+    
+    jsonFile.open('r');                                                 //Open the JSON file for reading
+    var str = jsonFile.read();                                          //Read everything and store it in variable 'str'
+    jsonFile.close();  
+    
+    return JSON.parse(str);
+}
+
+function saveJPEG(name) {
+    var doc = app.activeDocument;
+    var file = new File(doc.path + '/Output/' + name + '.jpg');                //Creates and specifies the path of the file which will be saved
+    
+    var opts = new JPEGSaveOptions();                                   //Tells PS the details of how to save the JPEG File
+    opts.quality = 10;                                                  //Sets Quality of JPEG output file to 10
+    
+    doc.saveAs(file, opts, true);                                       //Saves the file
+}
